0d008b1e0037c8bb4a90b4e3a1dc52ef0cf30e53
Hsieh Ichen <ichen761108@gmail.com>
2017-02-09 20:01:35 +0900

    kadai

---
+++ 08 のコピー/detail.php
@@ -0,0 +1,79 @@
+<?php
+$id = $_GET["id"];
+//1.  DB接続します
+try {
+  $pdo = new PDO('mysql:dbname=gs_db;charset=utf8;host=localhost','root','');
+} catch (PDOException $e) {
+  exit('データベースに接続できませんでした。'.$e->getMessage());
+}
+
+//２．データ登録SQL作成
+$stmt = $pdo->prepare("SELECT * FROM gs_user_table WHERE id = :id");
+$stmt->bindValue(':id',$id,PDO::PARAM_INT);//STR or INT
+$status = $stmt->execute();
+
+//３．データ表示
+$view="";
+if($status==false){
+  //execute（SQL実行時にエラーがある場合）
+  $error = $stmt->errorInfo();
+  exit("ErrorQuery:".$error[2]);
+}else{
+ $res = $stmt->fetch();
+    
+}
+?>
+    <!DOCTYPE html>
+    <html lang="ja">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>POSTデータ登録</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <style>
+            div {
+                padding: 10px;
+                font-size: 16px;
+            }
+        </style>
+    </head>
+
+    <body>
+
+        <!-- Head[Start] -->
+        <header>
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header"><a class="navbar-brand" href="bm_list_view.php">データ一覧</a></div>
+            </nav>
+        </header>
+        <!-- Head[End] -->
+
+        <!-- Main[Start] -->
+        <form method="post" action="bm_update.php">
+            <div class="jumbotron">
+                <fieldset>
+                    <legend>フリーアンケート</legend>
+                    <label>名前：
+                        <input type="text" name="name" value="<?=$res["name"]?>">
+                    </label>
+                    <br>
+                    <label>アカウント：
+                        <input type="text" name="lid" value="<?=$res["lid"]?>">
+                    </label>
+                    <br>
+                    <label>パスワード
+                        <input type="password" name="lpw" value="<?=$res["lpw"]?>">
+                    </label>
+                    <br>
+                    <input type="text" name="id" value="<?=$id?>">
+                    <input type="submit" value="送信">
+                </fieldset>
+            </div>
+        </form>
+        <!-- Main[End] -->
+
+
+    </body>
+
+    </html>
