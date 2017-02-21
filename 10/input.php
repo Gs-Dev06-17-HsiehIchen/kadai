0523ddf849a324e37d54e6431803152868c9b43d
Hsieh Ichen <ichen761108@gmail.com>
2017-02-21 11:36:50 +0900

    jQuery IMG Upload Preview

--- 10/input.php
+++ 10/input.php
@@ -2,6 +2,7 @@
 session_start();
 include("functions.php");
 ssidCheck();
+
 ?>
 
 
@@ -12,7 +13,49 @@ ssidCheck();
   <title>POSTデータ登録</title>
   <script src="ckeditor/ckeditor.js"></script>
   <link href="css/bootstrap.min.css" rel="stylesheet">
-  <style>div{padding: 10px;font-size:16px;}</style>
+  <style>
+      div{padding: 10px;font-size:16px;}
+    </style><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<!--jQuery IMG Upload Preview-->
+<script>
+$(function(){
+	var setFileInput = $('.imgInput');
+
+	setFileInput.each(function(){
+		var selfFile = $(this),
+		selfInput = $(this).find('input[type=file]');
+
+		selfInput.change(function(){
+			var file = $(this).prop('files')[0],
+			fileRdr = new FileReader(),
+			selfImg = selfFile.find('.imgView');
+
+			if(!this.files.length){
+				if(0 < selfImg.size()){
+					selfImg.remove();
+					return;
+				}
+			} else {
+				if(file.type.match('image.*')){
+					if(!(0 < selfImg.size())){
+						selfFile.append('<img alt="" class="imgView">');
+					}
+					var prevElm = selfFile.find('.imgView');
+					fileRdr.onload = function() {
+						prevElm.attr('src', fileRdr.result);
+					}
+					fileRdr.readAsDataURL(file);
+				} else {
+					if(0 < selfImg.size()){
+						selfImg.remove();
+						return;
+					}
+				}
+			}
+		});
+	});
+});
+</script>
 </head>
 <body>
 
@@ -33,7 +76,13 @@ ssidCheck();
      <script>
      CKEDITOR.replace("editor1");
      </script>
+     
+     <div id="container">
+     <div class="imgInput">
      <input type="file" name="filename">
+    </div>
+    </div>
+    
      <input type="submit" value="記事登録">
     </fieldset>
   </div>
